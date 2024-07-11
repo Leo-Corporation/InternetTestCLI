62d9953b1d1a8664da80a2c1fee3377f6b175287
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-11 11:46:24 +0200

    Added DownDetector command

---
+++ InternetTestCLI/Commands/DownDetector.cs
@@ -0,0 +1,43 @@
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Exceptions;
+using CliFx.Infrastructure;
+using PeyrSharp.Core;
+
+namespace InternetTestCLI.Commands;
+
+[Command("downdetector", Description = "Checks if a website is down.")]
+public class DownDetectorTestCommand() : ICommand
+{
+    [CommandParameter(0, Name = "site", Description = "Site URL.")]
+    public required string Site { get; init; }
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        try
+        {
+            Console.WriteLine($"Test in progress for {Site}, please wait...");
+
+
+            var statusInfo = await Internet.GetStatusInfoAsync(Site); // Makes a request to the specified website and saves the status code and message
+            var color = statusInfo.StatusCode switch
+            {
+                var code when code >= 100 && code < 200 => ConsoleColor.Blue,// Informational
+                var code when code >= 200 && code < 300 => ConsoleColor.Green,// Success
+                var code when code >= 300 && code < 400 => ConsoleColor.Yellow,// Redirection
+                var code when code >= 400 && code < 500 => ConsoleColor.Red,// ClientError
+                var code when code >= 500 && code < 600 => ConsoleColor.DarkRed,// ServerError
+                _ => Console.ForegroundColor,// Keep the default color for unexpected values
+            };
+            // Load Information
+            Console.Write("Status Code: "); Console.ForegroundColor = color; Console.Write(statusInfo.StatusCode); Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine($"Status Type: {statusInfo.StatusType}");
+            Console.Write($"Status Message: "); Console.ForegroundColor = color; Console.Write(statusInfo.StatusDescription); Console.ResetColor();
+        }
+        catch (Exception ex)
+        {
+            throw new CommandException(ex.Message);
+        }
+    }
+}
