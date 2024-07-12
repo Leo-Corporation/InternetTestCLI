085b5716cc4d93ad148bede0fed129b787f4f2a3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-12 12:55:32 +0200

    Added new executable name

--- InternetTestCLI/Program.cs
+++ InternetTestCLI/Program.cs
@@ -4,6 +4,8 @@ public static class Program
 {
     public static async Task<int> Main() =>
         await new CliApplicationBuilder()
+            .SetTitle("InternetTest CLI")
+            .SetDescription("InternetTest CLI is a command line interface that provides the same features of InternetTest Pro. It can locate IP addresses, send ping request, get DNS information and more!")
             .AddCommandsFromThisAssembly()
             .Build()
             .RunAsync();
