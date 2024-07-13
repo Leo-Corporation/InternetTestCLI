cbdd1e23def1d138665af2f46a9a5b854104c3f7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-13 13:53:49 +0200

    Fixed null collection in DNS command

--- InternetTestCLI/Commands/Dns.cs
+++ InternetTestCLI/Commands/Dns.cs
@@ -39,65 +39,57 @@ public class DnsCommand() : ICommand
 
     private void GetDnsInfo(string website)
     {
+        // Get WHOIS
+        var whois = new WhoisLookup();
+        var response = whois.Lookup(website);
 
-        try
+        // Get IP
+        IPHostEntry host = Dns.GetHostEntry(website);
+        IPAddress ip = host.AddressList[0];
+
+        Console.WriteLine($"IP Address: {ip}");
+        Console.WriteLine("");
+
+        // Get DNS records
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"DNS Records");
+        Console.WriteLine($"===========");
+        Console.ResetColor();
+        var lookup = new LookupClient();
+        var result = lookup.QueryAsync(website, QueryType.ANY).Result;
+        foreach (var record in result.AllRecords)
         {
-            // Get WHOIS
-            var whois = new WhoisLookup();
-            var response = whois.Lookup(website);
-
-            // Get IP
-            IPHostEntry host = Dns.GetHostEntry(website);
-            IPAddress ip = host.AddressList[0];
-
-            Console.WriteLine($"IP Address: {ip}");
-            Console.WriteLine("");
-
-            // Get DNS records
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"DNS Records");
-            Console.WriteLine($"===========");
-            Console.ResetColor();
-            var lookup = new LookupClient();
-            var result = lookup.QueryAsync(website, QueryType.ANY).Result;
-            foreach (var record in result.AllRecords)
-            {
-                if (RecordTypes.Contains(record.RecordType))
-                    Console.WriteLine($"{record.RecordType} - {record}");
-            }
-
-            // Display WHOIS
-            Console.WriteLine("");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"WHOIS");
-            Console.WriteLine($"=====");
-            Console.ResetColor();
-
-            if (Advanced)
-            {
-                Console.WriteLine(response.Content);
-            }
-            else
-            {
-                Console.WriteLine($"Creation: {response.Registered}");
-                Console.WriteLine($"Expires: {response.Expiration}");
-                Console.WriteLine($"Status:\n\t{string.Join("\n\t", response.DomainStatus)}");
-                string regInfo = "";
-                foreach (var prop in response.Registrant.GetType().GetProperties())
-                {
-                    var val = prop.GetValue(response.Registrant, null);
-                    if (val is null || prop.Name == "Address") continue;
-                    regInfo += $"\t{prop.Name} - {val}\n";
-                }
-                Console.WriteLine("Registrant Information:");
-                Console.WriteLine(regInfo);
-            }
-
+            if (RecordTypes is not null && !RecordTypes.Contains(record.RecordType)) continue;
+            Console.WriteLine($"{record.RecordType} - {record}");
         }
-        catch (Exception e)
+
+        // Display WHOIS
+        Console.WriteLine("");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"WHOIS");
+        Console.WriteLine($"=====");
+        Console.ResetColor();
+
+        if (Advanced)
         {
-            Console.WriteLine(e.Message);
+            Console.WriteLine(response.Content);
         }
+        else
+        {
+            Console.WriteLine($"Creation: {response.Registered}");
+            Console.WriteLine($"Expires: {response.Expiration}");
+            Console.WriteLine($"Status:\n\t{string.Join("\n\t", response.DomainStatus)}");
+            string regInfo = "";
+            foreach (var prop in response.Registrant.GetType().GetProperties())
+            {
+                var val = prop.GetValue(response.Registrant, null);
+                if (val is null || prop.Name == "Address") continue;
+                regInfo += $"\t{prop.Name} - {val}\n";
+            }
+            Console.WriteLine("Registrant Information:");
+            Console.WriteLine(regInfo);
+        }
+
     }
 
 }
