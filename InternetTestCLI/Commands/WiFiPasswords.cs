0eb6c58f270a854b8dba19791a1e0c1d35ddf811
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-14 12:14:09 +0200

    Added License header

--- InternetTestCLI/Commands/WiFiPasswords.cs
+++ InternetTestCLI/Commands/WiFiPasswords.cs
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
 #if _WINDOWS
 using System.Diagnostics;
 using System.Xml.Serialization;
@@ -24,7 +48,7 @@ public class WiFiPasswordCommand() : ICommand
             Console.Output.WriteLine($"Retrieving your WiFi Passwords, please wait...\n");
 
             await GetWiFiNetworksInfo();
-            LoadWiFiInfo(FileSys.AppDataPath + @"\Léo Corporation\InternetTest CLI\WiFis", Name);
+            LoadWiFiInfo(FileSys.AppDataPath + @"\LÃ©o Corporation\InternetTest CLI\WiFis", Name);
 
         }
         catch (Exception ex)
@@ -37,7 +61,7 @@ public class WiFiPasswordCommand() : ICommand
     {
 
         // Check if the temp directory exists
-        string path = FileSys.AppDataPath + @"\Léo Corporation\InternetTest CLI\WiFis";
+        string path = FileSys.AppDataPath + @"\LÃ©o Corporation\InternetTest CLI\WiFis";
 
         if (!Directory.Exists(path))
         {
