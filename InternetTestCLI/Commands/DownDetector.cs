ffa4e72e6da82bd9d4f68c95d24c2467b041465a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-01-04 14:53:51 +0100

    Added the possibility to test multiple sites with DownDetector (#5)

--- InternetTestCLI/Commands/DownDetector.cs
+++ InternetTestCLI/Commands/DownDetector.cs
@@ -27,47 +27,52 @@ using CliFx.Attributes;
 using CliFx.Exceptions;
 using CliFx.Infrastructure;
 using PeyrSharp.Core;
+using System;
 
 namespace InternetTestCLI.Commands;
 
 [Command("downdetector", Description = "Checks if a website is down.")]
 public class DownDetectorTestCommand() : ICommand
 {
-    [CommandParameter(0, Name = "site", Description = "Site URL.")]
-    public required string Site { get; init; }
+    [CommandParameter(0, Name = "sites", Description = "Site URLs.")]
+    public required string[] Sites { get; init; }
 
     public async ValueTask ExecuteAsync(IConsole Console)
     {
         try
         {
-            string url = Site;
-            if (!(url.Contains("https://") || url.Contains("http://")))
+            foreach (var site in Sites)
             {
-                Console.Output.WriteLine("The protocol was not specified, assuming that it is HTTPS.");
-                url = "https://" + url;
-            }
-            Console.Output.WriteLine($"Test in progress for {url}, please wait...");
+				string url = site;
+				if (!(url.Contains("https://") || url.Contains("http://")))
+				{
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.Output.WriteLine("The protocol was not specified, assuming that it is HTTPS.");
+					url = "https://" + url;
+				}
+				Console.Output.WriteLine($"Test in progress for {url}, please wait...");
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Output.WriteLine("\nStatus");
-            Console.Output.WriteLine("======\n");
-            Console.ResetColor();
+				Console.ForegroundColor = ConsoleColor.Cyan;
+				Console.Output.WriteLine($"\n{url}");
+				Console.Output.WriteLine(new string('=', url.Length));
 
-            var statusInfo = await Internet.GetStatusInfoAsync(url); // Makes a request to the specified website and saves the status code and message
-            var color = statusInfo.StatusCode switch
-            {
-                var code when code >= 100 && code < 200 => ConsoleColor.Blue,// Informational
-                var code when code >= 200 && code < 300 => ConsoleColor.Green,// Success
-                var code when code >= 300 && code < 400 => ConsoleColor.Yellow,// Redirection
-                var code when code >= 400 && code < 500 => ConsoleColor.Red,// ClientError
-                var code when code >= 500 && code < 600 => ConsoleColor.DarkRed,// ServerError
-                _ => Console.ForegroundColor,// Keep the default color for unexpected values
-            };
-            // Load Information
-            Console.Output.Write("Status Code: "); Console.ForegroundColor = color; Console.Output.Write(statusInfo.StatusCode); Console.ResetColor();
-            Console.Output.WriteLine("");
-            Console.Output.WriteLine($"Status Type: {statusInfo.StatusType}");
-            Console.Output.Write($"Status Message: "); Console.ForegroundColor = color; Console.Output.Write(statusInfo.StatusDescription + "\n"); Console.ResetColor();
+				Console.ResetColor();
+				var statusInfo = await Internet.GetStatusInfoAsync(url); // Makes a request to the specified website and saves the status code and message
+				var color = statusInfo.StatusCode switch
+				{
+					var code when code >= 100 && code < 200 => ConsoleColor.Blue,// Informational
+					var code when code >= 200 && code < 300 => ConsoleColor.Green,// Success
+					var code when code >= 300 && code < 400 => ConsoleColor.Yellow,// Redirection
+					var code when code >= 400 && code < 500 => ConsoleColor.Red,// ClientError
+					var code when code >= 500 && code < 600 => ConsoleColor.DarkRed,// ServerError
+					_ => Console.ForegroundColor,// Keep the default color for unexpected values
+				};
+				// Load Information
+				Console.Output.Write("Status Code: "); Console.ForegroundColor = color; Console.Output.Write(statusInfo.StatusCode); Console.ResetColor();
+				Console.Output.WriteLine("");
+				Console.Output.WriteLine($"Status Type: {statusInfo.StatusType}");
+				Console.Output.Write($"Status Message: "); Console.ForegroundColor = color; Console.Output.Write(statusInfo.StatusDescription + "\n"); Console.ResetColor();
+			}
         }
         catch (Exception ex)
         {
